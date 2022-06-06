10764a075b00e5661fa9b4a684380ceebb3359fd
drakembite <drakembite@gmail.com>
2022-06-06 00:35:50 -0500

    avances

--- base-del-conocimiento.pl
+++ base-del-conocimiento.pl
@@ -1,31 +1,18 @@
 pertenece_al('programador',['diseñar','codificar','mantener']).
 pertenece_al(puesto,tareas).
 
-identificar(A,B):-
-pertenece_al(A,[_|C]):, pertenece_al(A,C) 
+identificar(A,B).
 
-Desarrollo web -> Desarrollador backend, frontend
-Ciencia de datos
-
-trabajo_especialista(desattolloWeb,desarrolloBackend):- conocimientos(node;express).
+pertenece_al(A,[_|C]):- pertenece_al(A,C).
+conociemintos(_).
+pertenece_al('programador',['diseñar','codificar','mantener']):- conocimientos(node;express).
 
 
+pertenece_al('Ingeniero en seguridad informatica', ['mantener la seguridad', 'detectar virus y amenazas', 'detectar y analizar malwares', 'atender emergencias']):-
+  conocimientos(python;javascript;wireshark;johnTheRipper;nmap;burtSuite;ncat;criptografia;redes).
+
+pertenece_al('Desarrollador Backend', ['crear aplicaciones de almacenamiento de datos', 'optimizar aplicaciones', 'codificar aplicaciones de servidor', 'establecer bases de datos', 'realizar APIs']):-
+  conocmientos(php;python;ruby;java;net;javascript;golap;rust;mysql;postgres;mongodb;firestore;cassandra;amazonDynamoDB;apacheHBase,git;node;express:apisoap;apirest;apirestful).
 
 
-
-
-
-
-
-tipo(hongos,enfermedad).
-tipo(virus,enfermedad).
-tipo(bacteria,enfermedad).
-
-enfermedad_tipo(rancha,hongo).
-enfermedad_tipo(sarna_plateada,hongo).
-enfermedad_tipo(erwinia,bacteria).
-enfermedad_tipo(moteado_andino,virus).
-enfermedad_tipo(virosis,virus).
-
-un_tipo(A,J):- enfermedad_tipo(A,J).
-un_tipo(A,J):- enfermedad_tipo(A,J),enfermedad_tipo(J,A). 
+pertenece_al( )
